9cdbd88249a217b7372a1fcebd55778ad0676ee7
hzy <1054539066@qq.com>
2018-01-14 22:39:51 +0800

    创建

---
+++ health/src/main/java/hzy/dao/LogMapper.java
@@ -0,0 +1,17 @@
+package hzy.dao;
+
+import hzy.entity.Log;
+
+public interface LogMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Log record);
+
+    int insertSelective(Log record);
+
+    Log selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Log record);
+
+    int updateByPrimaryKey(Log record);
+}
---
+++ health/src/main/java/hzy/entity/Log.java
@@ -0,0 +1,75 @@
+package hzy.entity;
+
+import java.util.Date;
+
+public class Log {
+    private Integer id;
+
+    private Integer operatorId;
+
+    private String operatorName;
+
+    private String operate;
+
+    private String target;
+
+    private Integer targetId;
+
+    private Date operateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOperatorId() {
+        return operatorId;
+    }
+
+    public void setOperatorId(Integer operatorId) {
+        this.operatorId = operatorId;
+    }
+
+    public String getOperatorName() {
+        return operatorName;
+    }
+
+    public void setOperatorName(String operatorName) {
+        this.operatorName = operatorName == null ? null : operatorName.trim();
+    }
+
+    public String getOperate() {
+        return operate;
+    }
+
+    public void setOperate(String operate) {
+        this.operate = operate == null ? null : operate.trim();
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target == null ? null : target.trim();
+    }
+
+    public Integer getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(Integer targetId) {
+        this.targetId = targetId;
+    }
+
+    public Date getOperateTime() {
+        return operateTime;
+    }
+
+    public void setOperateTime(Date operateTime) {
+        this.operateTime = operateTime;
+    }
+}
