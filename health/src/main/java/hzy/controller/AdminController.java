c83ef9fed1ed67386bb39b5463c1ecc273633b9a
hzy <1054539066@qq.com>
2018-01-19 19:52:44 +0800

    添加adminLogin

--- health/src/main/java/hzy/controller/AdminController.java
+++ health/src/main/java/hzy/controller/AdminController.java
@@ -1,9 +1,12 @@
 package hzy.controller;
 
+import hzy.entity.Admin;
 import hzy.service.IAdminSer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import utils.Result;
 
 @Controller
 @RequestMapping("admin")
@@ -16,4 +19,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
     @Autowired
     private IAdminSer adminSer;
+
+    @RequestMapping("adminLogin")
+    @ResponseBody
+    public Result adminLogin(Admin admin){
+        Result result ;
+        Admin one = adminSer.selectAdminByNameAndPsd(admin);
+        if(one != null){
+            result = new Result(true, one);
+        }else{
+            result = new Result(false);
+        }
+        return result;
+    }
 }
